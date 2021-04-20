014fd52f10f2f515f696354812c6f89ee65cd1b8
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-20 11:37:25 -0700

    used promises instead of XML and cleaned up code

--- src/main.js
+++ src/main.js
@@ -8,24 +8,31 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+  let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
       }
-    };
-
+    }
     request.open("GET", url, true);
     request.send();
+  });
 
-    function getElements(response) {
-      $('.showHumidity').html(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').html(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      $('.showWind').html(`The wind speed is currently ${response.wind.speed}`);
-      $('.showConditions').html(`The current conditions of your city are ${response.weather[0].main}`);
-    }
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+      $('.showHumidity').html(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').html(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.showWind').html(`The wind speed is currently ${body.wind.speed}`);
+      $('.showConditions').html(`The current conditions of your city are ${body.weather[0].main}`);
+      $('.showErrors').text("");
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('.showHumidity').text("");
+      $('.showTemp').text("");
+    });
   });
 });
