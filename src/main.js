9be4228c8a036d358d8c621fef49cb19c690c21a
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-20 12:45:47 -0700

    take out promise, add it to new .js page, and refactor into a static method

--- src/main.js
+++ src/main.js
@@ -8,20 +8,6 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-  let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(request.response);
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
-
   promise.then(function(response) {
     const body = JSON.parse(response);
       $('.showHumidity').html(`The humidity in ${city} is ${body.main.humidity}%`);
